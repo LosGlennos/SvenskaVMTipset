a60ab6506aaa2eec5a89fa42b11e9fd8fb8fe793
Martin Svensson <martinhugosvensson@gmail.com>
2016-07-01 15:36:17 +0200

    First commit

---
+++ src/SvenskaVMTipset.Web/app/app.component.ts
@@ -0,0 +1,7 @@
+﻿import { Component } from '@angular/core';
+import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from 'angular2/router';
+@Component({
+  selector: 'my-app',
+  templateUrl: 'app/app.component.html'
+})
+export class AppComponent { }
