9fb0d1e997ab860eba4cc0de8174c079585d9a27
Martin Svensson <martinhugosvensson@gmail.com>
2016-07-05 16:41:11 +0200

    Starting over, this shit wont hold..

--- src/SvenskaVMTipset.Web/app/app.component.ts
+++
@@ -1,7 +0,0 @@
-﻿import { Component } from '@angular/core';
-import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from 'angular2/router';
-@Component({
-  selector: 'my-app',
-  templateUrl: 'app/app.component.html'
-})
-export class AppComponent { }
